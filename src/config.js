2fecf2a1a008f6a248330d35af3cad10ee986ca0
Su Yun <manxisuo@gmail.com>
2019-04-11 02:37:06 +0800

    增加分页功能

--- src/config.js
+++ src/config.js
@@ -3,7 +3,13 @@ const REPO = 'manxisuo/blog'
 const TITLE = 'Manxisuo的博客'
 const ABOUT_NUMBER = '1' // About页的issue序号
 const API_PREFIX = 'https://api.github.com'  // API前缀
+const PAGE_SIZE = 10 // 文章列表中每页显示的文章数
 
 export {
-    AUTHOR, REPO, TITLE, ABOUT_NUMBER, API_PREFIX
+    AUTHOR,
+    REPO,
+    TITLE,
+    ABOUT_NUMBER,
+    API_PREFIX,
+    PAGE_SIZE,
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,11 @@
     <template v-if="posts">
       <template v-if="posts.length > 0">
         <PostMeta v-for="post in posts" :key="post.id" v-bind="post" />
+        <el-pagination
+          layout="prev, pager, next" background
+          @current-change="changePage"
+          :current-page="page" :page-size="pageSize" :total="total"
+          class="gh-pagination"></el-pagination>
       </template>
       <el-alert v-else title="不存在匹配的文章" type="info" />
     </template>
@@ -11,7 +16,7 @@
 
 <script>
 import axios from 'axios'
-import {AUTHOR, REPO, API_PREFIX} from '@/config.js'
+import {AUTHOR, REPO, API_PREFIX, PAGE_SIZE} from '@/config.js'
 import util from '@/util.js'
 import PostMeta from '@/components/PostMeta.vue'
 
@@ -22,46 +27,81 @@ export default {
   },
   data () {
     return {
-      posts: null
+      posts: null,
+      pageSize: PAGE_SIZE,
+      page: 1,
+      total: 0
     }
   },
   mounted() {
-    const fn = (items) => {
-      this.posts = items.map(item => ({
-        id: item.id,
-        number: item.number,
-        title: item.title,
-        url: item.url,
-        time: util.convertTime(item.created_at),
-        commentCount: item.comments
-      }))
-    }
-
-    const kw = this.$route.query.kw
-    if (kw) {
-      this.doSearch(kw, fn)
-    }
-    else {
-      this.doQueryAll(fn)
-    }
+    this.page = parseInt(this.$route.query.page) || 1
+    this.load()
   },
   methods: {
+    load() {
+      const fn = (items) => {
+        this.posts = items.map(item => ({
+          id: item.id,
+          number: item.number,
+          title: item.title,
+          url: item.url,
+          time: util.convertTime(item.created_at),
+          commentCount: item.comments
+        }))
+      }
+
+      const kw = this.$route.query.kw
+      if (kw) {
+        this.doSearch(kw, fn)
+      }
+      else {
+        this.doQueryAll(fn)
+      }
+    },
     doSearch (kw, fn) {
       const url = `${API_PREFIX}/search/issues`
       axios.get(url, {
         params: {
-          q: `${kw} user:${AUTHOR} repo:${REPO} author:${AUTHOR} type:issue in:title,body`
+          q: `${kw} user:${AUTHOR} repo:${REPO} author:${AUTHOR} type:issue in:title,body`,
+          per_page: PAGE_SIZE,
+          page: this.page
         }
       }).then(resp => {
+        this.fetchTotal(resp.headers['link'])
         fn(resp.data.items)
       })
     },
     doQueryAll (fn) {
       const url = `${API_PREFIX}/repos/${REPO}/issues`
-      axios(url).then(resp => {
+      axios(url, {
+        params: {
+          per_page: PAGE_SIZE,
+          page: this.page
+        }
+      }).then(resp => {
+        this.fetchTotal(resp.headers['link'])
         fn(resp.data)
       })
+    },
+    changePage (page) {
+      this.$router.push({path: '', query: {page: page}})
+    },
+    fetchTotal (link) {
+      let groups = link.match(/per_page=(\d+)&page=(\d+)>; rel="last"/)
+      if (groups) {
+        this.total = parseInt(groups[1]) * parseInt(groups[2])
+      }
+      else {
+        groups = link.match(/per_page=(\d+)&page=(\d+)>; rel="prev"/)
+        this.total = parseInt(groups[1]) * parseInt(groups[2]) + 1
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+.gh-pagination {
+  text-align: center;
+}
+</style>
